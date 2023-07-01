5c09b006be96dffb25456e5312ce32fe55b866c1
Joaquin I. Corradi (Fedora Linux) <corradijoaquin@gmail.com>
2023-07-01 00:48:07 -0300

    JS en contacto funcionando

---
+++ assets/js/contacto.js
@@ -0,0 +1,35 @@
+function borrar() {
+  document.getElementById("nombre").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("mensaje").value = "";
+}
+
+function mostrarMensaje() {
+  let nombreDelCliente = document.getElementById("nombre").value;
+  let emailDelCliente = document.getElementById("email").value;
+
+  alert(
+    "Hola " +
+      nombreDelCliente +
+      ", te vamos a responder lo antes posible a través de " +
+      emailDelCliente
+  );
+
+  borrar();
+}
+
+function validarEntrada() {
+  nombreDelCliente = document.getElementById("nombre").value;
+  emailDelCliente = document.getElementById("email").value;
+  mensajeDelCliente = document.getElementById("mensaje").value;
+
+  if (
+    nombreDelCliente == "" ||
+    emailDelCliente == "" ||
+    mensajeDelCliente == ""
+  ) {
+    alert("Por favor, complete todos los campos");
+  } else {
+    mostrarMensaje();
+  }
+}
